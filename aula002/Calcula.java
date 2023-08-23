4259b51513fbe16e80ec784c894928580235a414
Eduardo da Silva Sichelero <135671476+eduardosichelero@users.noreply.github.com>
2023-08-22 21:23:27 -0300

    Update Calcula.java

---
+++ aula002/Calcula.java
@@ -0,0 +1,12 @@
+package aula002;
+
+public class Calcula {
+    public static void main(String[] args) {
+        int num1 = 3;
+        num1 = 5;
+        num1++;
+        int num2 = num1 / 2;
+
+        System.out.println(num2);
+    }
+}
